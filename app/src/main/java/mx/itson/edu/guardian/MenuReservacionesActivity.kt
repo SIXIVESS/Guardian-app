9b559562a5791739778bc6fe31024bdfaa981300
Aldo <aldomendivil17@gmail.com>
2024-04-18 02:24:17 -0700

    reservaciones

---
+++ app/src/main/java/mx/itson/edu/guardian/MenuReservacionesActivity.kt
@@ -0,0 +1,11 @@
+package mx.itson.edu.guardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuReservacionesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_reservaciones)
+    }
+}
