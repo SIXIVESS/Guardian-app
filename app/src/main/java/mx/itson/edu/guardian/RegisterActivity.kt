e03c3339941c43bb1a0df0e6b9d4256523cfd0d9
SIXIVESS <mettatonbb@gmail.com>
2024-04-17 18:39:38 -0700

    registro normal

--- app/src/main/java/mx/itson/edu/guardian/RegisterActivity.kt
+++ app/src/main/java/mx/itson/edu/guardian/RegisterActivity.kt
@@ -1,21 +1,39 @@
 package mx.itson.edu.guardian
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
-
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
-        // Set up the "Registrarse" button
-        val registerButton = findViewById<Button>(R.id.button3)
-        registerButton.setOnClickListener {
-            // Start the MenuActivity when the button is clicked
-            startActivity(Intent(this, MenuActivity::class.java))
+        val button : Button = findViewById(R.id.registerButton)
+        val buttonDos : Button = findViewById(R.id.clickHereComp)
+
+        button.setOnClickListener {
+            var intent: Intent = Intent( this, LoginActivity::class.java)
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Cuenta creada")
+            builder.setMessage("Tu cuenta ha sido creada. Por favor, inicia sesión.")
+            builder.show()
+            startActivity(intent)
         }
+
+            buttonDos.setOnClickListener {
+            var intent: Intent = Intent( this, RegisterCompActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
+
     }
 }
