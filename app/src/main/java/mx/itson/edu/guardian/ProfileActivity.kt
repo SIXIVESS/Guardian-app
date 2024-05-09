c1ed5534a301b5ac50634b608fe459a986bfd97b
Jorge Andrés Figueroa Márquez <jorge.figueroa228692@potros.itson.edu.mx>
2024-05-08 23:00:04 -0700

    feat: Profile User information deployment
    XD

--- app/src/main/java/mx/itson/edu/guardian/ProfileActivity.kt
+++ app/src/main/java/mx/itson/edu/guardian/ProfileActivity.kt
@@ -2,10 +2,35 @@ package mx.itson.edu.guardian
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.getValue
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        //val name: TextView = findViewById(R.id.userName)
+        val email: TextView = findViewById(R.id.userMail)
+
+        database = FirebaseDatabase.getInstance()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null) {
+            val userEMail = currentUser.email
+
+            email.text = userEMail
+        } else {
+            email.text = "Usuario no autenticado"
+        }
     }
 }
